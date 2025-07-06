d6547fb48577907a99715711775e3c4e522728d9
Om Prakash Pant <ompant229@gmail.com>
2025-07-06 14:29:24 +0545

    adding project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/app/about/About";
 import Hero from "@/app/hero/Hero";
+import Project from "@/components/project";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About/>
+      <Project/>
     </>
   )
 }
